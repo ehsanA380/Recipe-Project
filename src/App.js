4613a90fdf380b9e564709f156d51139cf497d34
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-02-17 23:37:47 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+
+// let history = useHistory();
+
 
 function App() {
-  async  function  fetchApi (){
-  const view = document.querySelector('#view');
-       fetch('http://localhost:3001/auth/login')
-             .then(response=>response.json())
-            .then(json=>{
-              
-              if(view.innerHTML==""){
-                for (const key in json) {
-                if (json.hasOwnProperty.call(json, key)) {
-                  // const element = json[key];
-                  view.innerHTML += `<li>${key} : ${json[key]}</li>`;
-                  
-                }
-              }
-              }
-              console.log(json);
-            })
-            .catch(err=>{
-              console.error(err);
-            })
-  }
-  return (
-    <div className="App">
-      <button onClick={fetchApi}>fetch Api</button>
-      <div id="view"></div>
-    </div>
-  );
-}
+ 
+  
+    
+    return (
+      <>
+        <h1>Starting form here</h1>
+      </>
+   
+      
+  )
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   async  function  fetchApi (){
+  //   const view = document.querySelector('#view');
+  //        fetch('http://localhost:3001/auth/login')
+  //              .then(response=>response.json())
+  //             .then(json=>{
+
+  //               if(view.innerHTML==""){
+  //                 for (const key in json) {
+  //                 if (json.hasOwnProperty.call(json, key)) {
+  //                   // const element = json[key];
+  //                   view.innerHTML += `<li>${key} : ${json[key]}</li>`;
+
+  //                 }
+  //               }
+  //               }
+  //               console.log(json);
+  //             })
+  //             .catch(err=>{
+  //               console.error(err);
+  //             })
+  //   }
+
+
+
+
+
+
+
+
+     
