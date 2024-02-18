4f3d2242ddc181b50b13840b10f105630d3a84f2
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-02-18 22:39:47 +0530

    clearing all the neccessory file of this project

--- src/App.js
+++ src/App.js
@@ -1,68 +1,8 @@
-// import logo from './logo.svg';
-import "./App.css";
-import React, { useState } from "react";
-
-
-// let history = useHistory();
-
-
+import  './App.css'
 function App() {
- 
-  
-    
-    return (
-      <>
-        <h1>Starting form here</h1>
-      </>
-   
-      
+  return (
+    <div className='App'></div>
   )
-};
+}
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //   async  function  fetchApi (){
-  //   const view = document.querySelector('#view');
-  //        fetch('http://localhost:3001/auth/login')
-  //              .then(response=>response.json())
-  //             .then(json=>{
-
-  //               if(view.innerHTML==""){
-  //                 for (const key in json) {
-  //                 if (json.hasOwnProperty.call(json, key)) {
-  //                   // const element = json[key];
-  //                   view.innerHTML += `<li>${key} : ${json[key]}</li>`;
-
-  //                 }
-  //               }
-  //               }
-  //               console.log(json);
-  //             })
-  //             .catch(err=>{
-  //               console.error(err);
-  //             })
-  //   }
-
-
-
-
-
-
-
-
-     
+export default App
