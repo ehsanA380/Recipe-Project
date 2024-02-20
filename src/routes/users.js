70e7df293bc58ded531412897e9bd0d21369a179
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2024-02-20 21:39:57 +0530

    alt: updatig the users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,40 +5,43 @@ import {UserModel} from '../modules/UserModel.js' // dont forget to add .js exte
 
 const router = express.Router();
 
+
+// api for registration ______________________________
 router.post('/register', async (req,res)=>{
   const {username, password} = req.body;
   const user = await UserModel.findOne({username});
   if(user){
     return res.json({message:"user already exists!"})
   }
-   
   // decritpting the password
   const hashedPassword = await bcrypt.hash(password,10);
-
+// saving the document into the database {username,password}
   const newUser = new UserModel({username,password: hashedPassword});
   await newUser.save();
-
   res.json({message:"User Registered Successfully."})
 })
 
+
+// Api for login -________________________________________
 router.post('/login', async (req,res)=>{
   const {username,password}=req.body;
   const user = await UserModel.findOne({username});
-
   if(!user){
-    return res.json({message:"User doesn't Exists!"})
+    return res.json({message:"User doesn't Exists!",loggedStatus:false})
   }
-
+// checking the password
   const isPasswordValid = await bcrypt.compare(password,user.password);
-
   if(!isPasswordValid){
-    return res.json({message:"Username or Password Is Incorret!"})
+    return res.json({message:"Username or Password Is Incorret!",loggedStatus:false})
   }
-  const token = jwt.sign({id:user._id},"secrect")
-  res.json({token,userID:user._id})
+  // generating token
+  if(user){
+    const token = jwt.sign({id:user._id},"secrect")
+    res.json({token,userID:user._id,name:user.username,message:"logged in successfully!",loggedStatus:true})
 
-
+  }
 })
+
 
 export {router as userRouter}
 
