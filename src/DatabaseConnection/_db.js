fc3c86a47397bdf01a116a1506c755cf6744434f
Ehsan Ansari <123573237+ehsanA380@users.noreply.github.com>
2024-03-01 20:10:43 +0530

    Update _db.js
    alt:dbConnetionpatth

--- src/DatabaseConnection/_db.js
+++ src/DatabaseConnection/_db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 // database connection 
 async function db() {
+ const connpath ="mongodb+srv://ehsanansari123123:gC6T6n71wzjhhixN@mernstactappdatabase.yprxhlw.mongodb.net/?retryWrites=true&w=majority"
  const conn= await mongoose.connect("mongodb://127.0.0.1:27017/recipes");
  
     if(conn){
